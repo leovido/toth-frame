b5e37e93c050b0bf18aa8ad35080125c7beb1ab9
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-06-01 13:22:52 +0100

    chore: update config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			// We are in the client build, so we exclude these Node.js modules
-			config.resolve.fallback = {
-				fs: false,
-				net: false,
-				tls: false,
-				http2: false,
-				dns: false
-			};
-		}
-
-		return config;
+	reactStrictMode: false,
+	images: {
+		remotePatterns: [
+			{
+				hostname: "*",
+				protocol: "http"
+			},
+			{
+				hostname: "*",
+				protocol: "https"
+			}
+		]
 	}
 };
 
